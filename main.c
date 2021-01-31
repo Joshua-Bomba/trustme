e09a41e1466eeed386423b582aa7d70a353051e5
Joshua Bomba <snipy67@hotmail.com>
2021-01-31 17:24:10 -0500

    Hmmm not getting any com port. Let's turn on the led to let us know the code ran

--- main.c
+++ main.c
@@ -2,9 +2,18 @@
 #include "pico/stdlib.h"
 #include "stdbool.h"
 
+const uint ON_DEVICE_LED_PIN_GPIO = 25;
+
+void turn_signal_on() {
+    gpio_init(ON_DEVICE_LED_PIN_GPIO);
+    gpio_set_dir(ON_DEVICE_LED_PIN_GPIO, GPIO_OUT);
+    gpio_put(ON_DEVICE_LED_PIN_GPIO, 1);
+}
+
 int main()
 {
 	stdio_init_all();
+    turn_signal_on();
     while(true) {
         printf("Hello World!\n");
         sleep_ms(1000);
