0004c170380c517a71c470524fa9671a060903ad
Joshua Bomba <snipy67@hotmail.com>
2021-01-31 17:22:22 -0500

    Okay it's doing nothing now but lets see if we can get serial to work

--- main.c
+++ main.c
@@ -1,5 +1,14 @@
+#include <stdio.h>
 #include "pico/stdlib.h"
+#include "stdbool.h"
+
 int main()
 {
-	
-}
+	stdio_init_all();
+    while(true) {
+        printf("Hello World!\n");
+        sleep_ms(1000);
+    }
+
+    return 0;
+} 
